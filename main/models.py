050d049789113c6cc686281a4e0d38bbff6c64ce
cocogenik <smw06075@gmail.com>
2019-02-27 01:02:04 -0500

    change model and view

--- main/models.py
+++ main/models.py
@@ -3,16 +3,36 @@ from django.db import models
 # Create your models here.
 
 class Company(models.Model):
+    # The name of the company
     company_name = models.CharField(max_length=200)
 
+    # The industrial nature of the company
+    company_industry_choice = (
+        ('SW', 'Software Driven'),
+        ('HW', 'Hardware Driven'),
+    )
+    company_industry = models.CharField(
+        max_length = 2,
+        choices = company_industry_choice,
+        default='SW',
+    )
+
     def __str__(self):
-        return self.company_name
+        seq = (self.company_name, self.company_industry)
+        return ' in '.join(seq)
 
 class Job(models.Model):
+    # Relationship with company
+    job_company = models.ForeignKey(Company,
+        on_delete=models.CASCADE)
+    # Specific Role that this job offers
     job_role = models.CharField(max_length=200)
+    # The location where this job is offered
     job_location = models.CharField(max_length=10)
+    # The description of the job
     job_description = models.TextField()
-    job_opened = models.DateTimeField("date opened")
+
+    job_opened = models.DateTimeField("Date inserted to DB")
 
     def __str__(self):
         return self.job_role
--- main/views.py
+++ main/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
 
+from .models import Company, Job
+from django.template import loader
+from django.http import Http404
+
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<strong>Hello</strong>, Django! I'm at main.")
+    company_list = Company.objects.order_by('-company_name')[:]
+    template = loader.get_template('main/index.html')
+    context = {
+        'company_name_list': company_list,
+    }
+    return HttpResponse(template.render(context, request))
 
+def detail(request, company_id):
+    try:
+        company = Company.objects.get(id=company_id)
+        jobs = company.job_set.all()
+    except Company.DoesNotExist:
+        raise Http404("I made it: Company does not exist")
+    template = loader.get_template('main/detail.html')
+    context = {'company':company, "jobs": jobs}
+
+    return HttpResponse(template.render(context, request))
+
+def description(request, company_id):
+    response = "You're looking at description of company id %s."
+    return HttpResponse(response % company_id)
+
+def industry(request, company_id):
+    response = "You're looking at industry of company id %s."
+    return HttpResponse(response % company_id)
